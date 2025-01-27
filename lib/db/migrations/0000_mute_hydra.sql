c636215a6d8c16de9776c5d98ba70116cca4b4cf
Jordan Abram <jordan16abram@gmail.com>
2025-01-27 15:27:27 +0000

    init

---
+++ lib/db/migrations/0000_mute_hydra.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"hashed_password" text NOT NULL,
+	"name" text,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
